f255dbd8108ecb2270ad7e969434be88b95a5cc2
Thibaut LE LEVIER <thibaut@lelevier.fr>
2013-01-09 01:45:20 +0100

    ocmock setup, working on test cases

--- Tests/UIDevice_NameFromDevice_Tests.h
+++ Tests/UIDevice_NameFromDevice_Tests.h
@@ -10,7 +10,9 @@
 
 @interface UIDevice_NameFromDevice_Tests : SenTestCase
 
+@property (strong, nonatomic) id currentDeviceMock;
 @property (strong, nonatomic) NSString *firstName;
 @property (strong, nonatomic) NSString *lastName;
+@property (strong, nonatomic) NSString *fullName;
 
 @end
--- Tests/UIDevice_NameFromDevice_Tests.m
+++ Tests/UIDevice_NameFromDevice_Tests.m
@@ -8,18 +8,46 @@
 
 #import "UIDevice_NameFromDevice_Tests.h"
 
+#import <OCMock/OCMock.h>
+
+#import "UIDevice+NameFromDevice.h"
+
 @implementation UIDevice_NameFromDevice_Tests
 
 -(void)setUp
 {
     self.firstName = @"John";
     self.lastName = @"Doe";
+    self.fullName = [NSString stringWithFormat:@"%@ %@",self.firstName,self.lastName];
+    
+    self.currentDeviceMock = [OCMockObject partialMockForObject:[UIDevice currentDevice]];
 }
 
 -(void)tearDown
 {
     self.firstName = nil;
     self.lastName = nil;
+    self.fullName = nil;
 }
+
+-(void)test_BasicENName_shouldReturnValues
+{
+    //unfinished
+//    NSString *format = @"%@ %@'s iPhone";
+//    
+//    NSString *fakeName = [NSString stringWithFormat:format,self.firstName,self.lastName];
+//    
+//    [[[self.currentDeviceMock stub] andReturn:fakeName] name];
+//    
+//    NSLog(@"%@",[self.currentDeviceMock name]);
+//    NSLog(@"%@",[self.currentDeviceMock fullNameFromDevice]);
+//    NSLog(@"%@",[self.currentDeviceMock firstName]);
+//    NSLog(@"%@",[self.currentDeviceMock lastName]);
+//    
+//    STAssertEqualObjects(self.fullName, [self.currentDeviceMock fullNameFromDevice], nil);
+//    STAssertEqualObjects(self.firstName, [self.currentDeviceMock firstName], nil);
+//    STAssertEqualObjects(self.lastName, [self.currentDeviceMock lastName], nil);
+}
+
 
 @end
